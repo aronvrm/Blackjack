05fccb3454818c7e378807197a1c0fcabbf0f90b
aronvrm <56795512+aronvrm@users.noreply.github.com>
2021-10-10 20:08:27 +0200

    commit

--- Blackjack.py
+++ Blackjack.py
@@ -60,11 +60,9 @@ def blackjack():
             self.kaarten.append(kaart)
             self.waarde += waarden[kaart.getallen]
             if kaart.getallen == 'Aas':
-                self.aas += 1
+                self.aas += 1  # add to self.aces
 
         def aanpassing_voor_aas(self):
-
-            # als de waarde >21 is verander naar een 1
             while self.waarde > 21 and self.aas:
                 self.waarde -= 10
                 self.aas -= 1
@@ -89,7 +87,7 @@ def blackjack():
 
             try:
                 chips.weddenschap = int(
-                    input("Hoeveel chips wil je inzetten? (je start met 100)  "))
+                    input("Hoeveel chips wil je inzetten? "))
             except ValueError:
                 print("Je moet wel een heel getal invoeren.")
             else:
@@ -110,7 +108,7 @@ def blackjack():
 
         while True:
             x = input(
-                'Doorgaan of stoppen? Klik op "h" om door te gaan of "s" om te stoppen  ')
+                '\nDoorgaan of stoppen? Klik op "h" om door te gaan of "s" om te stoppen  ')
 
             if x[0].lower() == 'h':
                 hit(deck, hand)
@@ -144,7 +142,7 @@ def blackjack():
             print(kaart)
         print('Dealers hand: {dealer.waarde}')
 
-        print("\n Spelers hand:")
+        print("\nSpelers hand:")
         for kaart in speler.kaarten:
             print(kaart)
         print('Spelers hand: {speler.waarde}')
@@ -170,9 +168,11 @@ def blackjack():
     def push(speler, dealer):
         print('Het is een gelijkspel, dus een Push. Je krijgt je inzet terug.')
 
+    spelers_chips = Chips()
+
     while True:
-        print('Welkom bij Blackjack! Kom zo dicht mogelijk bij de 21 zonder eroverheen te gaan!\n\
-    Dealer gaat door tot ze 17 bereikt. Azen tellen als 1 of 11.')
+        print('\nWelkom bij Blackjack! Kom zo dicht mogelijk bij de 21 zonder eroverheen te gaan!\nDealer gaat door tot ze 17 bereikt. Azen tellen als 1 of 11.')
+        print("\nJe chip totaal is: ", spelers_chips.totaal)
 
         # hand
         deck = Deck()
@@ -185,9 +185,6 @@ def blackjack():
         dealer_hand = Hand()
         dealer_hand.voeg_kaart_toe(deck.delen())
         dealer_hand.voeg_kaart_toe(deck.delen())
-
-        # chips
-        spelers_chips = Chips()
 
         # weddenschap
         neem_weddenschap(spelers_chips)
@@ -209,36 +206,36 @@ def blackjack():
 
         # speler bust niet, dealer speelt
 
-        if speler_hand.waarde <= 21:
-            while dealer_hand.waarde < 17:
-                hit(deck, dealer_hand)
+            if speler_hand.waarde <= 21:
+                while dealer_hand.waarde < 17:
+                    hit(deck, dealer_hand)
 
-            # show alle kaarten dealer
-            laat_alles_zien(speler_hand, dealer_hand)
+                # show alle kaarten dealer
+                laat_alles_zien(speler_hand, dealer_hand)
 
-            # verschillende winnen scenarios
-            if dealer_hand.waarde > 21:
-                dealer_bust(speler_hand, dealer_hand, spelers_chips)
-            elif dealer_hand.waarde > speler_hand.waarde:
-                dealer_wint(speler_hand, dealer_hand, spelers_chips)
-            elif dealer_hand.waarde < speler_hand.waarde:
-                speler_wint(speler_hand, dealer_hand, spelers_chips)
+                # verschillende winnen scenarios
+                if dealer_hand.waarde > 21:
+                    dealer_bust(speler_hand, dealer_hand, spelers_chips)
+                elif dealer_hand.waarde > speler_hand.waarde:
+                    dealer_wint(speler_hand, dealer_hand, spelers_chips)
+                elif dealer_hand.waarde < speler_hand.waarde:
+                    speler_wint(speler_hand, dealer_hand, spelers_chips)
+                else:
+                    push(speler_hand, dealer_hand)
+
+                    # informeren nieuwe chips
+
+            print("\nJe chip totaal is: ", spelers_chips.totaal)
+
+            nieuw_spel = input(
+                'Wil je nog een keer spelen? (y om nog een keer te spelen) ')
+
+            if nieuw_spel[0].lower() == 'y':
+                playing = True
+                continue
             else:
-                push(speler_hand, dealer_hand)
-
-                # informeren nieuwe chips
-
-        print('\nJe chipaantal is: {}'.format(spelers_chips.totaal))
-
-        nieuw_spel = input(
-            'Wil je nog een keer spelen? (y om nog een keer te spelen')
-
-        if nieuw_spel[0].lower() == 'y':
-            playing = True
-            continue
-        else:
-            ("Bedankt voor het spelen!")
-            break
+                ("Bedankt voor het spelen!")
+                break
 
 
 blackjack()
---
+++ blackjack_goed.py
@@ -0,0 +1,221 @@
+def blackjack():
+
+    import random
+
+    suits = ('Harten', 'Ruiten', 'Klaver', 'Schoppen')
+    ranks = ('Twee', 'Drie', 'Vier', 'Vijf', 'Zes', 'Zeven',
+             'Acht', 'Negen', 'Tien', 'Boer', 'Vrouw', 'Koning', 'Aas')
+    values = {'Twee': 2, 'Drie': 3, 'Vier': 4, 'Vijf': 5, 'Zes': 6, 'Zeven': 7, 'Acht': 8,
+              'Negen': 9, 'Tien': 10, 'Boer': 10, 'Vrouw': 10, 'Koning': 10, 'Aas': 11}
+
+    playing = True
+
+    class Card:
+
+        def __init__(self, suit, rank):
+            self.suit = suit
+            self.rank = rank
+
+        def __str__(self):
+            return self.rank + ' van ' + self.suit
+
+    class Deck:
+
+        def __init__(self):
+            self.deck = []  # start with an empty list
+            for suit in suits:
+                for rank in ranks:
+                    # build Card objects and add them to the list
+                    self.deck.append(Card(suit, rank))
+
+        def __str__(self):
+            deck_comp = ''  # start with an empty string
+            for card in self.deck:
+                deck_comp += '\n '+card.__str__()  # add each Card object's print string
+            return 'Het dek heeft:' + deck_comp
+
+        def shuffle(self):
+            random.shuffle(self.deck)
+
+        def deal(self):
+            single_card = self.deck.pop()
+            return single_card
+
+    class Hand:
+
+        def __init__(self):
+            self.cards = []  # start with an empty list as we did in the Deck class
+            self.value = 0   # start with zero value
+            self.aces = 0    # add an attribute to keep track of aces
+
+        def add_card(self, card):
+            self.cards.append(card)
+            self.value += values[card.rank]
+            if card.rank == 'Aas':
+                self.aces += 1  # add to self.aces
+
+        def adjust_for_ace(self):
+            while self.value > 21 and self.aces:
+                self.value -= 10
+                self.aces -= 1
+
+    class Chips:
+
+        def __init__(self):
+            self.total = 100  # This can be set to a default value or supplied by a user input
+            self.bet = 0
+
+        def win_bet(self):
+            self.total += self.bet
+
+        def lose_bet(self):
+            self.total -= self.bet
+
+    def take_bet(chips):
+
+        while True:
+            try:
+                chips.bet = int(
+                    input("Hoeveel chips wil je inzetten? "))
+            except ValueError:
+                print("Je moet wel een heel getal invoeren. ")
+            else:
+                if chips.bet > chips.total:
+                    print(
+                        "Sorry, je hebt niet genoeg geld om in te zetten! Je hebt: ", chips.total)
+                else:
+                    break
+
+    def hit(deck, hand):
+
+        hand.add_card(deck.deal())
+        hand.adjust_for_ace()
+
+    def hit_or_stand(deck, hand):
+        global playing  # to control an upcoming while loop
+
+        while True:
+            x = input(
+                "Doorgaan of stoppen? Klik op h om door te gaan of s om te stoppen  ")
+
+            if x[0].lower() == 'h':
+                hit(deck, hand)  # hit() function defined above
+
+            elif x[0].lower() == 's':
+                print("De speler stopt, het is de nu de dealers beurt.")
+                playing = False
+
+            else:
+                print("Sorry, je hebt geen correcte optie gekozen, probeer het opnieuw.")
+                continue
+            break
+
+    def show_some(player, dealer):
+        print("\nDealers hand:")
+        print("(Verborgen kaart)")
+        print('', dealer.cards[1])
+        print("\nSpelers hand:", *player.cards, sep='\n ')
+
+    def show_all(player, dealer):
+        print("\nDealers hand:", *dealer.cards, sep='\n ')
+        print("Dealer's hand =", dealer.value)
+        print("\nSpelers hand:", *player.cards, sep='\n ')
+        print("Spelers hand =", player.value)
+
+    def player_busts(player, dealer, chips):
+        print("Je bent gebust!")
+        chips.lose_bet()
+
+    def player_wins(player, dealer, chips):
+        print("Je wint!")
+        chips.win_bet()
+
+    def dealer_busts(player, dealer, chips):
+        print("De dealer bust, jij wint!")
+        chips.win_bet()
+
+    def dealer_wins(player, dealer, chips):
+        print("De dealer wint!")
+        chips.lose_bet()
+
+    def push(player, dealer):
+        print("Dealer en speler hebben een push! Je krijgt je inzet terug!.")
+
+    player_chips = Chips()  # remember the default value is 100
+
+    while True:
+        # Print an opening statement
+        print('\nWelkom bij Blackjack! Kom zo dicht mogelijk bij de 21 zonder eroverheen te gaan!\nDe dealer gaat door tot ze 17 bereikt. Azen tellen als 1 of 11.')
+        print("\nJe chip totaal is: ", player_chips.total)
+
+        # Create & shuffle the deck, deal two cards to each player
+        deck = Deck()
+        deck.shuffle()
+
+        player_hand = Hand()
+        player_hand.add_card(deck.deal())
+        player_hand.add_card(deck.deal())
+
+        dealer_hand = Hand()
+        dealer_hand.add_card(deck.deal())
+        dealer_hand.add_card(deck.deal())
+
+        # Prompt the Player for their bet
+        take_bet(player_chips)
+
+        # Show cards (but keep one dealer card hidden)
+        show_some(player_hand, dealer_hand)
+
+        while playing:  # recall this variable from our hit_or_stand function
+
+            # Prompt for Player to Hit or Stand
+            hit_or_stand(deck, player_hand)
+
+            # Show cards (but keep one dealer card hidden)
+            show_some(player_hand, dealer_hand)
+
+            # If player's hand exceeds 21, run player_busts() and break out of loop
+            if player_hand.value > 21:
+                player_busts(player_hand, dealer_hand, player_chips)
+                break
+
+        # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+
+            if player_hand.value <= 21:
+
+                while dealer_hand.value < 17:
+                    hit(deck, dealer_hand)
+                    hit(deck, dealer_hand)
+
+                # Show all cards
+                show_all(player_hand, dealer_hand)
+
+                # Run different winning scenarios
+                if dealer_hand.value > 21:
+                    dealer_busts(player_hand, dealer_hand, player_chips)
+
+                elif dealer_hand.value > player_hand.value:
+                    dealer_wins(player_hand, dealer_hand, player_chips)
+
+                elif dealer_hand.value < player_hand.value:
+                    player_wins(player_hand, dealer_hand, player_chips)
+
+                else:
+                    push(player_hand, dealer_hand)
+
+            # Inform Player of their chips total
+            print("\nJe chip totaal is: ", player_chips.total)
+
+            # Ask to play again
+            new_game = input(
+                "Klaar voor een nieuwe ronde? ")
+
+            if new_game[0].lower() == '':
+                playing = True
+                continue
+            else:
+                print("Bedankt voor het spelen!")
+                break
+
+
+blackjack()
