865c09050ea4e2f58dbf280a399f3d3d0e66c82b
aronvrm <56795512+aronvrm@users.noreply.github.com>
2021-11-01 18:17:33 +0100

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -21,9 +21,11 @@ def opening():
 
     # Opening van het verhaal
     print("Hoi", username, "leuk dat je deelneemt aan dit duurzame avontuur!")
-    type("Het is de 21e eeuw. De wereld bevindt zich momenteel in een klimaatcrisis!")
+    type("Het is de 21e eeuw. De wereld bevindt zich momenteel in een klimaatcrisis.")
     type("In dit verhaal gaan we je meenemen in een duurzaam alternatief voor jou pakketje.")
-    type("Namelijk crowd logistics, als jij deelneemt aan crowd logistics ga jij een pakketje bezorgen zonder veel extra om te rijden.")
+    type("Namelijk crowd logistics! ")
+    type("Als jij deelneemt aan crowd logistics ga jij een pakketje bezorgen terwijl je al onderweg bent.")
+    type("En dat zonder veel extra om te hoeven rijden.")
     type("Op deze manier hoeven er minder pakketbusjes te rijden, wat beter is voor de doorstroming en het mileu.")
 
 
@@ -36,7 +38,8 @@ keuze1 = True
 
 
 def beloning():
-    type("Je scrolt over je Facebook tijdlijn en je ziet een advertentie voorbij komen van Crowd to door.")
+    type("Je scrolt over je Facebook tijdlijn en je ziet een advertentie voorbijkomen van Crowd to door.")
+    type("Crowd to door is een online crowd logistics platform.")
     type("Je ziet dat je lekker kan bijverdienen door pakketjes af te leveren bij mensen als je toch al onderweg bent.")
     type("Dit is een leuke bijverdienste en je schrijft je dus in!")
     type("Binnen 2 dagen krijg je een oproep om een pakketje af te leveren als je uit je werk komt.")
@@ -45,7 +48,7 @@ def beloning():
     type("2 collega's zijn super enthousiast, ze vinden het een leuk verdienconcept en je draagt je steentje bij aan een duurzamere samenleving.")
     type("Een andere collega vindt het zeer nutteloos, je krijgt opmerkingen naar je hoofd zoals, daar heb je toch pakketbedrijven voor! Of verdien je nog niet voldoende?.")
     type("Je pauze is net voorbij en je krijgt de melding dat je binnen een uur moet beslissen of je het pakket gaat bezorgen. ")
-    type("Ga jij dat pakketje bezorgen?")
+    type("Ga jij dat pakketje bezorgen?")  # AANPASSEN
 
 # Hier ga je verder als je het op basis van intrinsieke motivatie al zou doen!
 
