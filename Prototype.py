46963d49fa754c31d66f3152858ee2e8f9852727
aronvrm <56795512+aronvrm@users.noreply.github.com>
2021-11-04 15:39:40 +0100

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -1,5 +1,6 @@
 from itertools import chain
 from time import sleep
+import webbrowser
 
 # typewriter effect
 KPS = 20  # Aantal karakters per seconde
@@ -29,11 +30,7 @@ def opening():
     type("Op deze manier hoeven er minder pakketbusjes te rijden, wat beter is voor de doorstroming en het mileu.")
 
 
-opening()
-
-
-keuze1 = True
-
+keuze1 = -True
 # Hier ga je verder als je een beloning nodig hebt
 
 
@@ -46,19 +43,31 @@ def beloning():
     type("Hiervoor ben je 20 minuten langer onderweg en levert het je 10 euro op.")
     type("Je hebt in de middagpauze met je collega's over het pakketje dat je kan bezorgen.")
     type("2 collega's zijn super enthousiast, ze vinden het een leuk verdienconcept en je draagt je steentje bij aan een duurzamere samenleving.")
-    type("Een andere collega vindt het zeer nutteloos, je krijgt opmerkingen naar je hoofd zoals, daar heb je toch pakketbedrijven voor! Of verdien je nog niet voldoende?.")
+    type("Een andere collega vindt het zeer nutteloos, je krijgt opmerkingen naar je hoofd zoals, daar heb je toch pakketbedrijven voor!")
     type("Je pauze is net voorbij en je krijgt de melding dat je binnen een uur moet beslissen of je het pakket gaat bezorgen. ")
-    type("Ga jij dat pakketje bezorgen?")  # AANPASSEN
+
+
+keuze2 = True
+
+
+def beloning_2():
+    type("Je pauze is net voorbij en je krijgt de melding dat je binnen een uur moet beslissen of je het pakket gaat bezorgen. ")
+
 
 # Hier ga je verder als je het op basis van intrinsieke motivatie al zou doen!
-
-
 def intrinsiek():
     type("In dit verhaal ga voor jou pakketje.")
 
 
+def einde():
+    type("Wat tof dat je hebt deelgenomen aan crowd logistics!")
+    type("Ondanks dat je bent afgehaakt willen je toch nog wat informatie meegeven rondom crowd logistics.")
+    print("\nDeze informatie wordt nu voor je geopend",
+          webbrowser.open('https://vil.be/en/project/crowd-logistics/'))
+
+
 # Keuze beloning of intrinsieke motivatie
-while keuze1:
+while keuze1 == True:
     keuze1 = input(
         "\nNu zijn wij heel benieuwd! Heb jij een vorm van beloning nodig om deel te nemem aan dit soort duurzame alternatieven? ")
     if keuze1 == 'ja':
@@ -68,3 +77,14 @@ while keuze1:
     else:
         type("dat is geen geldige keuze! je moet ja of nee kiezen")
         keuze1
+
+
+while keuze2:
+    keuze2 = input("Nu is de vraag, ga jij het pakketje bezorgen? ")
+    if keuze2 == "ja":
+        beloning_2()
+    if keuze2 == "nee":
+        einde()
+    else:
+        type("dat is geen geldige keuze! je moet ja of nee kiezen")
+        keuze2
